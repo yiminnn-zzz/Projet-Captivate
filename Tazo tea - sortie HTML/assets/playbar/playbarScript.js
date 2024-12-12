9269e0688c11a0a42a29cb25fddc76d84ff6a93b
yiminzhang <yiminzhang0@gmail.com>
2024-12-12 19:10:51 +0100

    Upload Captivate project files

---
+++ Tazo tea - sortie HTML/assets/playbar/playbarScript.js
@@ -0,0 +1,52 @@
+cp.playbarAssetArr = 
+[
+	'AudioOff',
+	'AudioOn',
+	'BackGround',
+	'Backward',
+	'Color',
+	'ColorSmall',
+	'CC',
+	'Exit',
+	'FastForward',
+	'FastForward1',
+	'FastForward2',
+	'Forward',
+	'Glow',
+	'GlowSmall',
+	'Height',
+	'InnerStroke',
+	'InnerStrokeSmall',
+	'Play',
+	'Pause',
+	'Progress',
+	'Rewind',
+	'Shade',
+	'ShadeSmall',
+	'Stroke',
+	'StrokeSmall',
+	'Thumb',
+	'ThumbBase',
+	'TOC'
+];
+cp.playbarTooltips = 
+{
+	AudioOff : "Audio activé ",
+	AudioOn : "Audio désactivé ",
+	Backward : "Retour ",
+	CC : "Sous-titrage ",
+	Exit : "Quitter ",
+	FastForward : "Vitesse d'avance rapide x2 ",
+	FastForward1 : "Vitesse d'avance rapide x4 ",
+	FastForward2 : "Vitesse normale ",
+	Forward : "Avancer ",
+	Play : "Lire ",
+	Pause : "Pause ",
+	Rewind : "Rembobiner ",
+	TOC : "Table des matières ",
+	Info : "Informations ",
+	Print : "Imprimer "
+};
+cp.handleSpecialForPlaybar = function(playbarConstruct)
+{
+}
